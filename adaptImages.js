88204f588116a4f56d46e8dd4f58aa0ff920acd3
Mrinal Wadhwa <mrinal@facto.rs>
2012-06-03 09:23:47 +0530

    removed underscores

--- adaptImages.js
+++ adaptImages.js
@@ -1,46 +1,46 @@
 function adaptImages(size){
   var
-    _doc = document,
-    _getElementsByTagName = 'getElementsByTagName',
-    _getAttribute = 'getAttribute',
-    _setAttribute = 'setAttribute',
-    _length = 'length',
-    _data = 'data-',
-    _alt = 'alt',
-    _nodes = _doc[_getElementsByTagName]('img'),
-    _sizes = ['l','m','s'],
-    _node,
-    _img,
-    _i,
-    _j;
+    doc = document,
+    getElementsByTagName = 'getElementsByTagName',
+    getAttribute = 'getAttribute',
+    setAttribute = 'setAttribute',
+    length = 'length',
+    data = 'data-',
+    alt = 'alt',
+    nodes = doc[getElementsByTagName]('img'),
+    sizes = ['l','m','s'],
+    node,
+    img,
+    i,
+    j;
   
-  size = _data + size;
-  a = _doc.body[_getAttribute](_data + 'img-sizes');
-  _sizes = a ? a.split(' ') : _sizes;
+  size = data + size;
+  a = doc.body[getAttribute](data + 'img-sizes');
+  sizes = a ? a.split(' ') : sizes;
   
-  for(_i = _nodes[_length]; _i--;) {
-    _node = _nodes[_i];
-    if(a = _node[_getAttribute](size))
-      _node[_setAttribute]('src', a);    
+  for(i = nodes[length]; i--;) {
+    node = nodes[i];
+    if(a = node[getAttribute](size))
+      node[setAttribute]('src', a);    
   }
   
   for(
-    _nodes = _doc[_getElementsByTagName]('noscript'), _i = _nodes[_length];
-    _i--;
+    nodes = doc[getElementsByTagName]('noscript'), i = nodes[length];
+    i--;
   ) {
-    _node = _nodes[_i];
-    if(a = _node[_getAttribute](size)) {
-      _img = _doc.createElement('img');
-      _img[_setAttribute]('src', a);
+    node = nodes[i];
+    if(a = node[getAttribute](size)) {
+      img = doc.createElement('img');
+      img[setAttribute]('src', a);
       
-      for(_j = _sizes[_length]; _j--;)
-        if(a = _node[_getAttribute](_data + _sizes[_j]))
-          _img[_setAttribute](_data + _sizes[_j], a);
+      for(j = sizes[length]; j--;)
+        if(a = node[getAttribute](data + sizes[j]))
+          img[setAttribute](data + sizes[j], a);
       
-      if(a = _node[_getAttribute](_data + _alt))
-        _img[_setAttribute](_alt, a);
+      if(a = node[getAttribute](data + alt))
+        img[setAttribute](alt, a);
       
-      _node.parentNode.replaceChild(_img, _node);
+      node.parentNode.replaceChild(img, node);
     }
   }
 }
